bf0bb81ef689bfd6e6e95d8d6e5640b45271418c
komarinko <keserapaseri8214@gmail.com>
2019-04-14 01:59:27 +0900

    [Finish] 5-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a936a7370995fc7e644910330492ff1df1555417d5455d4050ce7e1ffad5e9ab4c866f2b7dd340fb4e78da816a64bf700ba28c9416b2ee9b343192241fdf4c01'
