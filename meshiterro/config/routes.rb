0524a712668e6386fcdc7a5dfac723c889a1ff8b
komarinko <keserapaseri8214@gmail.com>
2019-04-14 22:58:41 +0900

    [Finish] 5-5

--- meshiterro/config/routes.rb
+++ meshiterro/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   	resource :favorites, only: [:create, :destroy]
   	resource :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show]
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
